d79779f92a128a834af87369e198e3e7b9fc802f
DanielV33 <vianadiasdaniel2@gmail.com>
2023-08-27 16:14:16 -0300

    Create decompondo.py

---
+++ decompondo.py
@@ -0,0 +1,8 @@
+num=input("Digite o numero:")
+lista=[]
+for i in num:
+    lista.append(i)
+print("Unidade: {0}".format(lista[3]))
+print("Dezena:  {0}".format(lista[2]))
+print("Centena: {0}".format(lista[1]))
+print("Milhar:  {0}".format(lista[0]))
